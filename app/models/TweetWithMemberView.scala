7f884f6899c2d5642d05aff881fcebb95b79b686
佐藤 有斗 <yuito.sato@BIZ0015000353.local>
2017-06-07 20:31:42 +0900

    add TweetWithMemberView

---
+++ app/models/TweetWithMemberView.scala
@@ -0,0 +1,28 @@
+package models
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+/**
+  * MEMBERテーブルの情報を付加させたTWEETテーブルのケースクラス
+  *
+  * @author yuito.sato
+  */
+case class TweetWithMemberView(
+  tweetId: Long,
+  memberId: Long,
+  tweetText: String,
+  registerDatetime: java.sql.Timestamp,
+  memberName: String
+)
+
+object TweetWithMemberView {
+
+  implicit val tweetWithMemberViewWrites: Writes[TweetWithMemberView] = (
+    (__ \ "tweetId").write[Long] and
+      (__ \ "memberId").write[Long] and
+      (__ \ "tweetText").write[String] and
+      (__ \ "registerDatetime").write[java.sql.Timestamp] and
+      (__ \ "memberName").write[String]
+    ) (unlift(TweetWithMemberView.unapply))
+}
