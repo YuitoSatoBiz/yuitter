17e09885983d693ccd5ff3a0a702428eb4c08407
佐藤 有斗 <yuito.sato@BIZ0015000353.local>
2017-06-09 11:02:16 +0900

    fix indentency in TweetWithMemberView

--- app/formats/TweetWithMemberView.scala
+++ app/formats/TweetWithMemberView.scala
@@ -20,9 +20,9 @@ object TweetWithMemberView {
 
   implicit val tweetWithMemberViewWrites: Writes[TweetWithMemberView] = (
     (__ \ "tweetId").write[Long] and
-      (__ \ "memberId").write[Long] and
-      (__ \ "tweetText").write[String] and
-      (__ \ "registerDatetime").write[java.sql.Timestamp] and
-      (__ \ "memberName").write[String]
+    (__ \ "memberId").write[Long] and
+    (__ \ "tweetText").write[String] and
+    (__ \ "registerDatetime").write[java.sql.Timestamp] and
+    (__ \ "memberName").write[String]
     ) (unlift(TweetWithMemberView.unapply))
 }
