8081d9fa40b826bec403c820e953903faac3211a
佐藤 有斗 <yuito.sato@BIZ0015000353.local>
2017-06-09 15:10:05 +0900

    refactor TweetWithMemberView

--- app/formats/TweetWithMemberView.scala
+++ app/formats/TweetWithMemberView.scala
@@ -3,7 +3,6 @@ package formats
 import java.time.LocalDateTime
 
 import play.api.libs.json._
-import play.api.libs.functional.syntax._
 
 /**
   * MEMBERテーブルの情報を付加させたTWEETテーブルのケースクラス
@@ -20,11 +19,5 @@ case class TweetWithMemberView(
 
 object TweetWithMemberView {
 
-  implicit val tweetWithMemberViewWrites: Writes[TweetWithMemberView] = (
-    (__ \ "tweetId").write[Long] and
-    (__ \ "memberId").write[Long] and
-    (__ \ "tweetText").write[String] and
-    (__ \ "registerDatetime").write[LocalDateTime] and
-    (__ \ "memberName").write[String]
-    ) (unlift(TweetWithMemberView.unapply))
+  implicit val tweetWithMemberViewWrites: OWrites[TweetWithMemberView] = Json.writes[TweetWithMemberView]
 }
