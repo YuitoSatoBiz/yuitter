badc98c06357a73d4a617190aa6a724ac3e0eefd
佐藤 有斗 <yuito.sato@BIZ0015000353.local>
2017-06-09 15:07:46 +0900

    add create action to TweetController

--- app/controllers/TweetController.scala
+++ app/controllers/TweetController.scala
@@ -2,10 +2,13 @@ package controllers
 
 import javax.inject.Inject
 
+import formats.TweetForm
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json._
 import play.api.mvc.{Action, AnyContent, Controller}
 import services.TweetService
+
+import scala.concurrent.Future
 
 /**
   * TWEETテーブルにCRUD処理を行うコントローラー
@@ -22,6 +25,18 @@ class TweetController @Inject()(val tweetService: TweetService) extends Controll
   def list: Action[AnyContent] = Action.async { implicit rs =>
     tweetService.list().map { tweets =>
       Ok(Json.toJson(tweets))
+    }
+  }
+
+  def create: Action[JsValue] = Action.async(parse.json) { implicit rs =>
+    rs.body.validate[TweetForm].map { form =>
+      tweetService.create(form).map { _ =>
+        Ok(Json.obj("result" -> "success"))
+      }
+    }.recoverTotal { e =>
+      Future {
+        BadRequest(Json.obj("result" ->"failure", "error" -> JsError.toJson(e)))
+      }
     }
   }
 }
