ff7cbdc108ec3e72196b2a0626738ea61c9f8e79
佐藤 有斗 <yuito.sato@BIZ0015000353.local>
2017-06-09 09:05:12 +0900

    delete unnecessary trait and injected val in TweetController

--- app/controllers/TweetController.scala
+++ app/controllers/TweetController.scala
@@ -3,10 +3,8 @@ package controllers
 import javax.inject.Inject
 
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.libs.json._
 import play.api.mvc.{Action, AnyContent, Controller}
-import slick.driver.JdbcProfile
 import services.TweetService
 
 /**
@@ -14,7 +12,7 @@ import services.TweetService
   *
   * @author yuito.sato
   */
-class TweetController @Inject()(val dbConfigProvider: DatabaseConfigProvider, val tweetService: TweetService) extends Controller with HasDatabaseConfigProvider[JdbcProfile] {
+class TweetController @Inject()(val tweetService: TweetService) extends Controller {
 
   def list: Action[AnyContent] = Action.async { implicit rs =>
     tweetService.list().map { tweets =>
