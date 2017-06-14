e4a04a80385fa55e02922418b298ef256a189cab
佐藤 有斗 <yuito.sato@BIZ0015000353.local>
2017-06-14 10:51:23 +0900

    add bCrypt encoder

--- app/repositories/MemberRepositoryJDBC.scala
+++ app/repositories/MemberRepositoryJDBC.scala
@@ -2,10 +2,11 @@ package repositories
 
 import java.sql.Timestamp
 import java.time.LocalDateTime
+import javax.inject.Inject
 
 import formats.MemberCommand
 import models.Tables.{Account, Member}
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import scala.concurrent.ExecutionContext.Implicits.global
 import slick.driver.MySQLDriver.api._
 import models.Tables.{AccountRow, MemberRow}
@@ -16,7 +17,7 @@ import utils.Consts
   *
   * @author yuito.sato
   */
-class MemberRepositoryJDBC {
+class MemberRepositoryJDBC @Inject()(val bCrypt: BCryptPasswordEncoder) {
 
   def countByEmailAddress(emailAddress: String): DBIO[Int] = {
     Member.filter(_.emailAddress === emailAddress).length.result
@@ -27,7 +28,7 @@ class MemberRepositoryJDBC {
       memberId <- Member returning Member.map(_.memberId) += MemberRow(
         memberId = Consts.DefaultId,
         emailAddress = form.emailAddress,
-        password = form.password,
+        password = bCrypt.encode(form.password),
         registerDatetime = Timestamp.valueOf(LocalDateTime.now),
         updateDatetime = Timestamp.valueOf(LocalDateTime.now),
         versionNo = Consts.DefaultVersionNo
