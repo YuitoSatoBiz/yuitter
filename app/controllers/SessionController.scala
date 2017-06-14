c90e3a0f1f3acf1ba3a24a86ecab4d06c0cbd911
佐藤 有斗 <yuito.sato@BIZ0015000353.local>
2017-06-14 19:05:05 +0900

    fix create action in SessionController

--- app/controllers/SessionController.scala
+++ app/controllers/SessionController.scala
@@ -3,19 +3,22 @@ package controllers
 import java.util.UUID
 import javax.inject.Inject
 import formats.MemberCommand
+import play.api.Logger
 import play.api.libs.json.{JsError, JsValue}
 import play.api.mvc.{Action, Controller}
 import play.api.libs.json._
 import play.api.cache._
-import services.SessionService
+import services.MemberService
+import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 /**
   * ログイン処理をするコントローラー
   *
   * @author yuito.sato
   */
-class SessionController @Inject()(val sessionService: SessionService, val cache: CacheApi)(implicit ec: ExecutionContext) extends Controller {
+class SessionController @Inject()(val memberService: MemberService, val cache: CacheApi)(implicit ec: ExecutionContext) extends Controller {
 
   def create: Action[JsValue] = Action.async(parse.json) { implicit rs =>
     rs.body.validate[MemberCommand].fold(
@@ -25,13 +28,12 @@ class SessionController @Inject()(val sessionService: SessionService, val cache:
         }
       },
       valid = { form =>
-        sessionService.authenticate(form).map { member =>
+        memberService.authenticate(form).map { member =>
           val token = UUID.randomUUID().toString
           val memberId = member.memberId
-          cache.set(token, memberId)
+          cache.set(token, memberId, 30.days)
           Ok(Json.obj("result" -> "success")).withSession(
             "token" -> token)
-
         }.recover { case _ =>
           BadRequest(Json.obj("result" -> "failure", "error" -> "メールアドレスまたはパスワードが間違えています。"))
         }
