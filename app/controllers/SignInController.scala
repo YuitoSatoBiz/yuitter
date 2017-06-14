cedb311c91504ead0bd224c0c298f802ab671fa1
佐藤 有斗 <yuito.sato@BIZ0015000353.local>
2017-06-14 11:12:25 +0900

    add a route for sign_in

---
+++ app/controllers/SignInController.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+/**
+  * ログイン処理をするコントローラー
+  *
+  * @author yuito.sato
+  */
+class LoginController extends Controller {
+
+  def login() = Action.async {}
+}
