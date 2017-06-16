5c580892fb5ad388d7dcddf9201026cb52b3fd27
佐藤 有斗 <yuito.sato@BIZ0015000353.local>
2017-06-16 19:04:06 +0900

    add actions and routings for AccountController

---
+++ app/controllers/AccountController.scala
@@ -0,0 +1,23 @@
+package controllers
+
+/**
+  * AccountテーブルのCRUD処理をするコントローラー
+  *
+  * @author yuito.sato
+  */
+class AccountController {
+
+  def search = ???
+
+  def listFollowers(accountId: Long) = ???
+
+  def listFollowees(accountId: Long) = ???
+
+  def find(accountId: Long) = ???
+
+  def create = ???
+
+  def update(accountId: Long) = ???
+
+  def delete(accountId: Long) = ???
+}
